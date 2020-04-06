d1f5b7414afe547a20484f8d4bf3555146da0924
Danko Novovic <danko.novovic91@gmail.com>
2020-04-06 16:21:09 +0200

    master public

---
+++ precache-manifest.289674b96ab9908a9fad3c94a61b76d8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7423dc2b738b880566d720d51f20cd39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e99f45c4e06f44e9f55",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7e99f45c4e06f44e9f55",
+    "url": "/static/js/2.5456ae6d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5456ae6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a1c57fdc8d713a90b0f",
+    "url": "/static/js/main.520eff95.chunk.js"
+  },
+  {
+    "revision": "59e9f7849c4b534cf6af",
+    "url": "/static/js/runtime-main.a7598a32.js"
+  },
+  {
+    "revision": "bcf1d6931ef4e94e88f4684f7b4f3039",
+    "url": "/static/media/rsz_stockapp_logo_transparent.bcf1d693.png"
+  },
+  {
+    "revision": "a662f92909229a80d15162a556d4229e",
+    "url": "/static/media/slajd1.a662f929.png"
+  },
+  {
+    "revision": "ff66a1d95c3c93da024df40fd205cfa2",
+    "url": "/static/media/slajd2.ff66a1d9.png"
+  },
+  {
+    "revision": "a2d2f6a41f11621aab56189c4b9a8647",
+    "url": "/static/media/slajd3.a2d2f6a4.png"
+  },
+  {
+    "revision": "6101070d170e4b8217f6ac9024a0ad5a",
+    "url": "/static/media/slajd4.6101070d.png"
+  }
+]);
